20bf31a0e3c06cc675829055dd3e1b89253ad2be
zweros <414356480@qq.com>
2019-05-30 22:09:07 +0800

    URL 学习

--- Fighter/src/cn/szxy/url/TestURL.java
+++ Fighter/src/cn/szxy/url/TestURL.java
@@ -3,8 +3,17 @@ package cn.szxy.url;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * 学习 URL类的方法
+ * getProtocol 
+ * getHost
+ * getPath
+ * 
+ *
+ */
 public class TestURL {
 	public static void main(String[] args) throws MalformedURLException {
+		
 		URL url = new URL("https://zwer.xyz");
 		System.out.println(""+url.getProtocol());
 		System.out.println(""+url.getHost());
